78f185e01ef820a45850e5d3e75831bd6a44a0c5
peter <peterugwu64@gmail.com>
2023-05-12 11:10:28 +0100

    Fix rubocop

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_and_belongs_to_many :categories
-  
-    validates :name, presence: true, length: { maximum: 250 }
-    validates :icon, presence: true, length: { maximum: 500 }
+  belongs_to :author, class_name: 'User'
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :icon, presence: true, length: { maximum: 500 }
 end
